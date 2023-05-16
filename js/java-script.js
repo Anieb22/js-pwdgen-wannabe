306d0e846de92d29a89fc3b099faca0a8dbd98e0
Anieb22 <anielloverdino@gmail.com>
2023-05-16 17:19:00 +0200

    aggiunto javascript (esercizio concluso)

---
+++ js/java-script.js
@@ -0,0 +1,7 @@
+let nome = prompt('qual è il tuo nome?')
+
+let cognome = prompt('qual è il tuo cognome')
+
+let colore_preferito = prompt('qual è il tuo colore preferito?')
+
+document.getElementById('password').innerHTML = nome + cognome + colore_preferito + 21
